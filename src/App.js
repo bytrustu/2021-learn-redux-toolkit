294a365ca653b051996fb7b1eeb4518a8f9206f2
bytrustu <bytrustu@gmail.com>
2021-01-15 23:08:52 +0900

    createAction, createStore, createReducer

--- src/App.js
+++ src/App.js
@@ -1,57 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { createStore } from 'redux';
+import { configureStore, createAction, createReducer } from '@reduxjs/toolkit';
+import { useSelector } from 'react-redux';
 
 function App() {
+
+  // const INCREMENT = 'INCREMENT';
+  // const DECREMENT = 'DECREMENT';
+  //
+  // function increment() {
+  //   return { type: INCREMENT };
+  // }
+  //
+  // function decrement() {
+  //   return { type: DECREMENT };
+  // }
+
+  const increment = createAction('INCREMENT');
+  const decrement = createAction('DECREMENT');
+
+  // function counter(state = 0, action) {
+  //   switch (action.type) {
+  //     case increment.type:
+  //       return state + 1;
+  //     case decrement.type:
+  //       return state - 1;
+  //     default:
+  //       return state;
+  //   }
+  // }
+
+  const counter = createReducer(0, {
+    [increment]: state => state + 1,
+    [decrement]: state => state - 1,
+  })
+
+  const store = configureStore({
+    reducer: counter,
+  });
+
+  const onClickIncrementHandle = () => {
+    store.dispatch(increment());
+  };
+
+  const onClickDecrementHandle = () => {
+    store.dispatch(decrement());
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <>
+      <button onClick={onClickDecrementHandle}>-</button>
+      <button onClick={onClickIncrementHandle}>+</button>
+
+    </>
   );
 }
 
